d382911281838152cc54272a7afbb00419d258d1
benjfield <benjfield@hotmail.com>
2023-12-05 19:58:02 +0000

    Cleaned up the answer

--- day5.py
+++ day5.py
@@ -12,7 +12,7 @@ def seed_1(text):
             elif re.match(".*map:", line):
                 seed_maps.append([])
             else:
-                decoded_map = re.match("(\d+) (\d+) (\d+)", line)
+                decoded_map = re.match(r"(\d+) (\d+) (\d+)", line)
                 if decoded_map:
                     seed_maps[-1].append(
                         {
@@ -44,95 +44,66 @@ def seed_2(text):
                 for seed_pair in re.findall(r"(\d+) (\d+)", line):
                     seeds.append({
                         "start": int(seed_pair[0]),
-                        "range": int(seed_pair[1])
+                        "end": int(seed_pair[0]) + int(seed_pair[1]) -1
                     })
             elif re.match(".*map:", line):
                 seed_maps.append([])
             else:
-                decoded_map = re.match("(\d+) (\d+) (\d+)", line)
+                decoded_map = re.match(r"(\d+) (\d+) (\d+)", line)
                 if decoded_map:
                     seed_maps[-1].append(
                         {
-                            "destination": int(decoded_map.group(1)),
-                            "source": int(decoded_map.group(2)),
-                            "range": int(decoded_map.group(3)),
+                            "difference": int(decoded_map.group(2)) - int(decoded_map.group(1)),
+                            "source_start": int(decoded_map.group(2)),
+                            "source_end": int(decoded_map.group(2)) + int(decoded_map.group(3)) -1,
                         }
                     )
-
-    def sortByDestination(map):
-        return map["destination"]
-    
-    for i, seed_map in enumerate(seed_maps):
-        seed_map.sort(key=sortByDestination)
-
-    def sortByStart(seed):
-        return seed["start"]
-    
-    seeds.sort(key=sortByStart)
-    
-    def find_seed(seed_maps, input):
-        current_type = input
-        for seed_map in reversed(seed_maps):
-            for single_seed_map in seed_map:
-                current_destination_diff = current_type - single_seed_map["destination"]
-                if current_destination_diff >= 0 and current_destination_diff < single_seed_map["range"]:
-                    current_type = current_destination_diff + single_seed_map["source"]
-                    break
-                elif current_destination_diff < 0:
-                    break
-        
-        return current_type
-
-    def add_valid_seeds(boundary_seeds, seeds, possible_seed):
-        for seed in seeds:
-            current_start_diff = possible_seed - seed["start"]
-            if current_start_diff >= 0 and current_start_diff < seed["range"]:
-                boundary_seeds.append(possible_seed)
-            if current_start_diff < 0:
-                break
-
-    boundary_seeds = []
-
-    for i in range(len(seed_maps) -1, -1, -1):
-        add_valid_seeds(boundary_seeds, seeds, find_seed(seed_maps[:i+1], 0))
-        for single_seed_map in seed_maps[i]:
-            add_valid_seeds(boundary_seeds, seeds, 
-                find_seed(seed_maps[:i+1], single_seed_map["destination"] - 1)
-            )
-            add_valid_seeds(boundary_seeds, seeds, 
-                find_seed(seed_maps[:i+1], single_seed_map["destination"])
-            )
-            add_valid_seeds(boundary_seeds, seeds, 
-                find_seed(seed_maps[:i+1], single_seed_map["destination"] + single_seed_map["range"] - 1)
-            )
-            add_valid_seeds(boundary_seeds, seeds, 
-                find_seed(seed_maps[:i+1], single_seed_map["destination"] + single_seed_map["range"])
-            )
-
-    for seed in seeds:
-        boundary_seeds.append(seed["start"])
-        boundary_seeds.append(seed["start"])
-
-    boundary_locations=[]
-
     def sortBySource(map):
-        return map["source"]
+        return map["source_start"]
     
     for i, seed_map in enumerate(seed_maps):
         seed_map.sort(key=sortBySource)
 
-    for seed in boundary_seeds:
-        current_type = seed
-        for seed_map in seed_maps:
-            for single_seed_map in seed_map:
-                current_source_diff = current_type - single_seed_map["source"]
-                if current_source_diff >= 0 and current_source_diff < single_seed_map["range"]:
-                    current_type = current_source_diff + single_seed_map["destination"]
-                    break
-                elif current_source_diff < 0:
-                    break
-        boundary_locations.append(current_type)
+    these_ranges = seeds
 
-    return min(boundary_locations)
+    for seed_map in seed_maps:
+        next_ranges = []
+        for seed in these_ranges:
+            current_seed_start = seed["start"]
+            for single_seed_map in seed_map:
+                possible_start = max(single_seed_map["source_start"], current_seed_start)
+
+                if possible_start > seed["end"]:
+                    break
+
+                possible_end = min(single_seed_map["source_end"], seed["end"])
+
+                if possible_end >= possible_start:
+                    if current_seed_start < possible_start:
+                        next_ranges.append({
+                            "start": current_seed_start,
+                            "end": possible_start - 1
+                        })
+                    next_ranges.append({
+                        "start": possible_start - single_seed_map["difference"],
+                        "end": possible_end - single_seed_map["difference"]
+                    })
+                    current_seed_start = possible_end + 1
+
+            if current_seed_start < seed["end"]:
+                next_ranges.append({
+                    "start": current_seed_start,
+                    "end": seed["end"]
+                })
+
+        these_ranges = next_ranges.copy()
+    
+    def sortByStart(seed):
+        return seed["start"]
+            
+    next_ranges.sort(key=sortByStart)
+    
+    return next_ranges[0]["start"]
 
 seed_text = get_data(day=5, year=2023)
+print(seed_2(seed_text))
