99406c7d0292515ed9035fc094e62cc5a44a9b84
Ben Field <ben@catalyst-tech.uk>
2025-12-12 10:15:39 +0000

    done day 10

--- advent/year_2025/day_10.py
+++ advent/year_2025/day_10.py
@@ -1,10 +1,7 @@
-from typing import Self
 from advent.runner import register
-from advent.utils.split_text import create_char_grid
-from advent.utils.grid import Grid
-from dataclasses import dataclass
 import numpy as np
-from scipy import linalg, optimize
+from scipy import optimize
+import math
 
 def values_in_detail(detail: str):
     return [int(x) for x in detail[1:-1].split(",")]
@@ -24,26 +21,17 @@ def buttons_2(text):
                 button_coeff[light_index] = 1
             coeffs.append(button_coeff)
 
+        c = np.ones(len(coeffs), dtype=int)
+        a = np.transpose(np.array(coeffs))
+        b = target
+
         solution = optimize.linprog(
-            c=np.ones(len(details[1:-1])),
-            A_eq=np.transpose(np.array(coeffs)),
-            b_eq=np.copy(target),
+            c=c,
+            A_eq=a,
+            b_eq=b,
             integrality=1,
         )
-
-        solution_presses = np.array([int(x) for x in solution.x])
-
-        check_answer = np.matmul(
-            np.transpose(np.array(coeffs)),
-            solution_presses
-        )
-
-        if not np.array_equal(target, check_answer):
-            print(solution)
-            print(target)
-            print(check_answer)
-            raise Exception
         
-        totals += int(solution.fun)
+        totals += round(solution.fun)
 
     return totals
